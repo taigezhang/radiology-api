3b2a24e47223ffd9faae6f3bfc0372645f663c60
taige zhang <43013752+taigezhang@users.noreply.github.com>
2026-04-29 23:20:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-3.11.9
+from fastapi import FastAPI
+from schemas import PredictRequest, PredictResponse, PredictionItem
+from model import predict_batch
+
+app = FastAPI()
+
+
+@app.get("/")
+def health_check():
+    return {"status": "ok", "message": "POST JSON to /predict"}
+
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(request: PredictRequest):
+    """
+    Main prediction endpoint.
+    Expects:
+    {
+        "cases": [...]
+    }
+
+    Returns:
+    {
+        "predictions": [...]
+    }
+    """
+    predictions = []
+
+    for case in request.cases:
+        case_predictions = predict_batch(case)
+        predictions.extend(case_predictions)
+
+    return PredictResponse(predictions=predictions)
